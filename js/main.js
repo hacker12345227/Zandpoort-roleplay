ec0ed310194a36ac83c22148858b1b55595a7ad8
hacker12345227 <Jarno.eulen12@gmail.com>
2026-04-03 18:12:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// SCROLL ANIMATIONS
-const cards = document.querySelectorAll(".card");
-
-window.addEventListener("scroll", () => {
-  cards.forEach(card => {
-    const pos = card.getBoundingClientRect().top;
-
-    if (pos < window.innerHeight - 100) {
-      card.style.opacity = "1";
-      card.style.transform = "translateY(0)";
-    }
-  });
-});
-
 // NAVBAR SCROLL EFFECT
 const navbar = document.querySelector(".navbar");
 
